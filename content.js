aa85c27551a027ecb8a8507b1d32e529d232f1a9
Phạm Sỹ Trường <34733883+phamsytruong@users.noreply.github.com>
2021-05-06 12:21:32 +0700

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,19 @@
 
+setInterval( () => {
+
+  // [1] CLose Button
+  try {
+    document.getElementById('close-button').click();
+  }
+  catch (ex) {}
+
+  // [2] Auto replay
+  try {
+    if (document.getElementsByClassName('PlayButton Tuner__Control__Button Tuner__Control__Play__Button TunerControl')[0].getAttribute('data-qa').includes('play_button')) {
+      document.getElementsByClassName('PlayButton Tuner__Control__Button Tuner__Control__Play__Button TunerControl')[0].click();
+    }
+  }
+  catch (ex) {}
+
+  
+}, 3000)
