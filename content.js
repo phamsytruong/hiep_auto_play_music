88645c2da059eb7e636cd47eecb84db63c29ae69
Phạm Sỹ Trường <34733883+phamsytruong@users.noreply.github.com>
2021-05-06 21:25:21 +0700

    Update content.js

--- content.js
+++ content.js
@@ -9,80 +9,84 @@ setTimeout( () => {
     catch(ex) {
         alert('click ads error');
     }
-}, 9 * 1000);
+}, 10 * 1000);
+
+setTimeout( () => {
+    setInterval( () => {
+      // Click Login
+      try {
+        document.getElementsByClassName('SignupModalButtons__loginLink')[0].click();
+      }
+      catch(ex) {}
+      try {
+        document.getElementsByClassName('ButtonT3 ButtonT3--large ButtonT3--dark ButtonT3--full')[0].click();
+      }
+      catch(ex) {}
+
+      // [1] CLose  Button
+      try {
+        // close button
+        try {
+           document.querySelector('#region-coachmark iframe').contentWindow.document.querySelector('#close-button').click();    
+        }
+        catch(ex) {
+        }
+      }
+      catch (ex) {}
+
+      // resume
+      try {
+        document.getElementsByClassName('ButtonT3 ButtonT3--inline')[document.getElementsByClassName('ButtonT3 ButtonT3--inline').length - 1].click(); 
+      }
+      catch(ex) {}
+      // let me listen
+      try {
+        document.getElementsByClassName('SimStreamViolation__acceptLink')[0].click();
+      }
+      catch(ex) {}
 
 
-setInterval( () => {
-  // Click Login
-  try {
-    document.getElementsByClassName('SignupModalButtons__loginLink')[0].click();
-  }
-  catch(ex) {}
-  try {
-    document.getElementsByClassName('ButtonT3 ButtonT3--large ButtonT3--dark ButtonT3--full')[0].click();
-  }
-  catch(ex) {}
-  
-  // [1] CLose  Button
-  try {
-    // close button
-    try {
-       document.querySelector('#region-coachmark iframe').contentWindow.document.querySelector('#close-button').click();    
-    }
-    catch(ex) {
-    }
-  }
-  catch (ex) {}
-  
-  // resume
-  try {
-    document.getElementsByClassName('ButtonT3 ButtonT3--inline')[document.getElementsByClassName('ButtonT3 ButtonT3--inline').length - 1].click(); 
-  }
-  catch(ex) {}
-  // let me listen
-  try {
-    document.getElementsByClassName('SimStreamViolation__acceptLink')[0].click();
-  }
-  catch(ex) {}
-  
 
-  
-  // Get started
-  try {
-    document.getElementsByClassName('ButtonT3 ButtonT3--dark ButtonT3--block')[0].click();
-  }
-  catch(ex) {
-  }
-  
-  // Playlist
-  try {
-    document.getElementsByClassName('ButtonRow__button ButtonRow__button--play')[0].click();
-  }
-  catch(ex) {}
+      // Get started
+      try {
+        document.getElementsByClassName('ButtonT3 ButtonT3--dark ButtonT3--block')[0].click();
+      }
+      catch(ex) {
+      }
 
-  // [2] Auto replay
-  try {
-    if (document.getElementsByClassName('PlayButton Tuner__Control__Button Tuner__Control__Play__Button TunerControl')[0].getAttribute('data-qa').includes('play_button')) {
-      document.getElementsByClassName('PlayButton Tuner__Control__Button Tuner__Control__Play__Button TunerControl')[0].click();
-    }
-  }
-  catch (ex) {}
-  
-  // loop
-  try {
-    if (document.getElementsByClassName('RepeatButton Tuner__Control__Button Tuner__Control__Repeat__Button TunerControl')[0].getAttribute('aria-label').includes('Repeat off')) {
-      document.getElementsByClassName('RepeatButton Tuner__Control__Button Tuner__Control__Repeat__Button TunerControl')[0].click();
-    }
-  }
-  catch (ex) {}
-  
-  // random
-  try {
-    if (document.getElementsByClassName('ShuffleButton Tuner__Control__Button Tuner__Control__Shuffle__Button TunerControl')[0].getAttribute('aria-checked').includes('false')) {
-      document.getElementsByClassName('ShuffleButton Tuner__Control__Button Tuner__Control__Shuffle__Button TunerControl')[0].click();
-    }
-  }
-  catch (ex) {}
+      // Playlist
+      try {
+        document.getElementsByClassName('ButtonRow__button ButtonRow__button--play')[0].click();
+      }
+      catch(ex) {}
 
-  
-}, 30 * 1000)
+      // [2] Auto replay
+      try {
+        if (document.getElementsByClassName('PlayButton Tuner__Control__Button Tuner__Control__Play__Button TunerControl')[0].getAttribute('data-qa').includes('play_button')) {
+          document.getElementsByClassName('PlayButton Tuner__Control__Button Tuner__Control__Play__Button TunerControl')[0].click();
+        }
+      }
+      catch (ex) {}
+
+      // loop
+      try {
+        if (document.getElementsByClassName('RepeatButton Tuner__Control__Button Tuner__Control__Repeat__Button TunerControl')[0].getAttribute('aria-label').includes('Repeat off')) {
+          document.getElementsByClassName('RepeatButton Tuner__Control__Button Tuner__Control__Repeat__Button TunerControl')[0].click();
+        }
+      }
+      catch (ex) {}
+
+      // random
+      try {
+        if (document.getElementsByClassName('ShuffleButton Tuner__Control__Button Tuner__Control__Shuffle__Button TunerControl')[0].getAttribute('aria-checked').includes('false')) {
+          document.getElementsByClassName('ShuffleButton Tuner__Control__Button Tuner__Control__Shuffle__Button TunerControl')[0].click();
+        }
+      }
+      catch (ex) {}
+
+
+    }, 10 * 1000);
+}, 45 * 1000);
+
+
+
